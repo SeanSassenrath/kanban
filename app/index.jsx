76e013699a4392f8dda05d2b46a76a4e3c4d6fbd
SeanSassenrath <sean.sassenrath@gmail.com>
2015-11-19 20:32:10 -0800

    Set up env

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import './main.css'
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App.jsx';
+
+main();
+
+function main() {
+  const app = document.createElement('div')
+
+  document.body.appendChild(app);
+
+  ReactDom.render(<App />, app);
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var TARGET = process.env.npm_lifecycle_event;
+var merge = require('webpack-merge');
+
+require('es6-promise').polyfill();
+
+var common = {
+  entry: APP_PATH,
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: APP_PATH
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: APP_PATH
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'kanban app'
+    })
+  ]
+};
+
+
+if(TARGET === 'start' || !Target) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  })
+}
